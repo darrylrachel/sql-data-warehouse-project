8bce10622c75b943f2fa4e48ed8f0bffaeb2c281
darrylrachel <racheldarryl@icloud.com>
2025-12-12 11:08:47 -0800

    Derived columns: Create new columns based on calculations or transformations of existing columns

--- scripts/bronze/eda.sql
+++ scripts/bronze/eda.sql
@@ -43,6 +43,53 @@ SELECT DISTINCT cst_marital_status,
         END cst_marital_status
 FROM bronze.crm_cust_info;
 
+INSERT INTO silver.crm_prd_info (
+    prd_id,
+    prd_key,
+    cat_id,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_dt,
+    prd_end_dt)
+
+SELECT prd_id,
+       SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key, -- Extract product key
+       REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id, -- Extract category ID
+       prd_nm,
+       ISNULL(prd_cost, 0) AS prd_cost,
+       CASE UPPER(TRIM(prd_line))
+            WHEN 'M' THEN 'Mountain'
+            WHEN 'R' THEN 'Road'
+            WHEN 'S' THEN 'Other Sales'
+            WHEN 'T' THEN 'Touring'
+            ELSE 'n/a'
+       END AS prd_line, -- Map product line codes to descriptive values
+       CAST (prd_start_dt AS DATE) AS prd_start_dt,
+       CAST (
+            LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1
+            AS DATE) AS prd_end_dt -- Calculate end date as one day before the next start date
+FROM bronze.crm_prd_info;
+
+SELECT prd_id,
+       COUNT(*)
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL;
+
+
+SELECT prd_key,
+       REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+       SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key
+FROM bronze.crm_prd_info
+WHERE REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') NOT IN
+(SELECT DISTINCT cid FROM bronze.erp_px_cat_g1v2);
+
+
+
+
+
+
 -- Silver Layer
 SELECT cst_id,
        COUNT(*)
